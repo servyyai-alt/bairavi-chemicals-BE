0eae9c8336a64fb2504be47555d2d53e86a54781
leastactioncompany <leastactioncompany@gmail.com>
2026-05-18 17:57:58 +0530

    env updated

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+// require('dotenv').config();
+// const app = require('./server');
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
 require('dotenv').config();
-const app = require('./server');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Backend running...');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
